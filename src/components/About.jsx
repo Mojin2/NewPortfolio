64b6c0ea71570538fa7839d91c55b0385f0f68b6
Mojin2 <aiduriaaa@gmail.com>
2024-03-13 21:45:06 +0900

    main page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,17 @@
-import { motion } from "framer-motion-3d";
+import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
+import { MovingComponent } from "react-moving-text";
 
 export const About = (props) => {
   const { setSection } = props;
   return (
-    <div className="h-screen w-screen flex flex-col items-start justify-center p-32">
-      <h1 className="text-[80px] font-extrabold leading-snug text-slate-100 font-PixelThick text-shadow-md">
-        Hi, I'm
-        <br />
-        <span className="bg-white px-1 italic text-slate-950">Jeengyu.</span>
+    <div className="border-2 border-red-500 h-screen w-screen flex flex-col items-start justify-center pb-60 pl-32">
+      <h1 className="border-2 border-green-500 text-[80px] font-extrabold text-slate-950 font-PixelThick flex flex-col">
+        <div>Hi, I'm</div>
+        <div>JeenGyu.</div>
       </h1>
       <motion.p
-        className="text-[40px] text-slate-100 mt-4 font-semibold font-PixelThick text-shadow-md"
+        className="text-[40px] text-slate-950 mt-4 font-semibold font-PixelThick"
         // initial={{ opacity: 0, y: 25 }}
         // whileInView={{
         //   opacity: 1,
@@ -19,22 +19,9 @@ export const About = (props) => {
         //   transition: { duration: 1, delay: 0.5 },
         // }}
       >
-        <TypeAnimation
-          sequence={[1000, "I'm frontend developer"]}
-          cursor={false}
-        />
-        <TypeAnimation
-          sequence={[
-            2000,
-            "with Creative ideas",
-            1000,
-            "with Passionate attitude",
-            1000,
-            "with Cooperation with Team",
-          ]}
-        />
+        <div>I Am Frontend Developer</div>
       </motion.p>
-      <motion.div
+      {/* <motion.div
         className="grid grid-cols-2 text-slate-50 font-PixelThick text-4xl gap-x-6 gap-y-3 mt-8 pt-5"
         initial={{ opacity: 0, y: 25 }}
         whileInView={{
@@ -67,7 +54,14 @@ export const About = (props) => {
         >
           Contact
         </div>
-      </motion.div>
+      </motion.div> */}
+      <motion.button
+        className="px-3 py-5 bg-[#7f1423] font-PixelThick flex justify-center items-center text-slate-50 shadow-md text-3xl"
+        // onClick={() => setSection(3)}
+        whileTap={{ scale: 0.85 }}
+      >
+        Play with Me?
+      </motion.button>
     </div>
   );
 };
